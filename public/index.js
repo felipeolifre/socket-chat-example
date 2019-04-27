806df53a4b10a8c751c34667a260203b5b76dc5f
Felipe Olivera <felipe.oliverafreire@adroll.com>
2019-04-27 20:39:15 -0300

    Add nickname support

--- public/index.js
+++ public/index.js
@@ -1,20 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = io();
 
-  socket.on('user connection', message => {
+  socket.on('system message', message => {
     const listItem = document.createElement('li');
     listItem.style.fontStyle = 'italic';
     listItem.textContent = message;
     document.getElementById('messages').appendChild(listItem);
   });
 
-  socket.on('chat message', message => {
+  socket.on('chat message', ({ nickname, message }) => {
     const listItem = document.createElement('li');
-    listItem.textContent = message;
+    listItem.textContent = `${nickname}: ${message}`;
     document.getElementById('messages').appendChild(listItem);
   });
 
-  document.getElementById('form').addEventListener('submit', e => {
+  document.getElementById('join-form').addEventListener('submit', e => {
+    e.preventDefault(); // Prevents page reloading.
+    const nicknameInput = document.getElementById('nickname');
+    socket.emit('join', nicknameInput.value);
+    nicknameInput.value = '';
+
+    const joinDiv = document.getElementById('join');
+    joinDiv.style.display = 'none';
+    const chatDiv = document.getElementById('chat');
+    chatDiv.style.display = 'block';
+  });
+
+  document.getElementById('chat-form').addEventListener('submit', e => {
     e.preventDefault(); // Prevents page reloading.
     const messageInput = document.getElementById('message');
     socket.emit('chat message', messageInput.value);
